a829d321db7d57377db01c3b10bfc005ae747045
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 07:28:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-textbox-graphics-widget',
@@ -9,6 +16,8 @@ setup(
     packages=find_packages(),
     description='PyQt text box which is movable, auto-resizable by text size. Parent class is QGraphicsWidget.',
     url='https://github.com/yjg30737/pyqt-textbox-graphics-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
